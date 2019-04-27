e1a8fc51d55657745dc017616170fe9b06881b67
Officium <iofficium@163.com>
2019-04-27 22:18:06 +0800

    add plot util

---
+++ src/common/plot.py
@@ -0,0 +1,48 @@
+""" Plot reward curve
+Usage: set xscale and run `python common/plot.py ../logs/ppo_* ../logs/trpo_*`
+"""
+import os
+import sys
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+xscale = {
+    'ppo': 1024 * 4,  # 1024 means timestep per update, 4 means skip4frame
+    'trpo': 4096 * 4,  # 4096 means timestep per update, 4 means skip4frame
+}
+COLORS = [
+    'blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 'purple',
+    'pink', 'brown', 'orange', 'teal',  'lightblue', 'lime', 'lavender',
+    'turquoise', 'darkgreen', 'tan', 'salmon', 'gold', 'darkred', 'darkblue'
+]
+eprews = dict()
+for path in sys.argv[1:]:
+    alg, seed = os.path.split(path)[-1].split('_')
+    if eprews.get(alg) is None:
+        eprews[alg] = []
+    eprews[alg].append([])
+    nan = 0
+    with open(path) as f:
+        for line in f:
+            if line.__contains__('eprewmean'):
+                rew = line.strip().rsplit(':', 1)[1].strip()
+                if rew == 'nan':
+                    nan += 1
+                else:
+                    eprews[alg][-1].extend([float(rew)] * (nan + 1))
+                    nan = 0
+
+f, axarr = plt.subplots(1, len(eprews), sharex=False, squeeze=False)
+for i, (alg, rews) in enumerate(eprews.items()):
+    ax = axarr[0][i]
+    min_length = min(len(rs) for rs in rews)
+    xs = np.arange(min_length) * xscale[alg]
+    rews = np.asarray([rs[:min_length] for rs in rews])
+    mean = np.mean(rews, 0)
+    std = np.std(rews, 0) / np.sqrt(len(rews))
+    ax.set_title(alg)
+    ax.plot(xs, mean, color=COLORS[i])
+    ax.fill_between(xs, mean - std, mean + std, color=COLORS[i], alpha=.4)
+plt.show()
